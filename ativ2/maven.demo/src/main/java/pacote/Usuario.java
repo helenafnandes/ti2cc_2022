26f3b9f897a7002886aeb789e5db957924524994
helenafnandes <103282849+helenafnandes@users.noreply.github.com>
2022-08-26 01:14:28 -0300

    Add files via upload
    atividade 2 
    ti2 - cc - PUC Minas
    2022

---
+++ ativ2/maven.demo/src/main/java/pacote/Usuario.java
@@ -0,0 +1,59 @@
+package pacote;
+
+
+public class Pessoa {
+	private int id;
+	private String nome;
+	private int idade;
+	
+	public Usuario() {
+		this.codigo = -1;
+		this.login = "";
+		this.senha = "";
+		this.sexo = '*';
+	}
+	
+	public Usuario(int codigo, String login, String senha, char sexo) {
+		this.codigo = codigo;
+		this.login = login;
+		this.senha = senha;
+		this.sexo = sexo;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public char getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [codigo=" + codigo + ", login=" + login + ", senha=" + senha + ", sexo=" + sexo + "]";
+	}	
+}
